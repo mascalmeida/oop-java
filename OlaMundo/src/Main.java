865c8ae481eb9312c48fee45417b4d895034afc0
mascalmeida <lucasmascalmeida@gmail.com>
2023-07-24 08:06:07 -0300

    feat[aula01&aula02]

---
+++ OlaMundo/src/Main.java
@@ -0,0 +1,9 @@
+// Todo objeto tem Atributos, Metodos e Estado
+// classe: define os atributos e metodos dos objetos
+// objeto: é uma instancia de uma classe
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
